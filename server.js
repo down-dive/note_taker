92b3964d7f0c10c1834c7db1dbe9537583a6eab5
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-04-16 21:09:48 -0700

    api calls are complete

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const { db } = require('./db/db.json')
+const notes = require('./db/db.json')
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-app.get('/api/db', (req, res) => {
-    res.json(db);
-  });
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
-  app.post('/api/db', (req, res) => {
+app.use(express.static('public'));
+
+function createNewPd(body, pdArray) {
+    const pd = body;
+    animalsArray.push(pd);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ animals: pdArray }, null, 2)
+    );
+    return animal;
+}
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+
+
+app.get('/api/notes', (req, res) => {
+    return res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
     // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-  });
+    let note = req.body
+    note.id = req.body.title
+    console.log(note);
+    notes.push(note)
+    res.json(note);
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server on port ${PORT}!`);
